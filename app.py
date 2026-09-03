52e918baeae04d8da36b1d858d7db8a59877cd76
Clement Dalo <clementdalo56@gmail.com>
2026-09-03 15:21:48 +0000

    mise

--- app.py
+++ app.py
@@ -182,8 +182,7 @@ else:
                     "Date_Saisie": date_saisie, "Date_Donnees": date_donnees, "Mois": maintenant.strftime("%B"), "Annee": maintenant.strftime("%Y"),
                     "Agent": agent_actif, "Categorie": "Données Extrêmes", "Type_Message_Fichier": "DONNEES EXTREMES", "Heure_Saisie": heure_saisie.strftime("%H:%M"),
                     "Statut_Delai": statut, "Details": contenu_texte
-
-                }
+                    
                 pd.concat([df, pd.DataFrame([nouvelle_ligne])], ignore_index=True).to_csv(FICHIER_BDD, index=False)
                 st.success(f"💾 Données du {date_donnees} enregistrées en local ! Statut : **{statut}**")
                 

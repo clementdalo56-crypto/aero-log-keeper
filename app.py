33f4257786d96b6001474584e9721640ffaded8e
Clement Dalo <clementdalo56@gmail.com>
2026-09-04 15:11:53 +0000

    vb

--- app.py
+++ app.py
@@ -467,9 +467,9 @@ else:
 
             with tab_podium:
                     st.markdown(f"### 🏆 Performances et Classement des Agents ({mois_sel} {annee_sel})")
-                if not df_temp.empty:
+                    if not df_temp.empty:
                     stats_ag = []
-                for ag in df_temp["Agent"].unique():
+                    for ag in df_temp["Agent"].unique():
                     df_ag = df_temp[df_temp["Agent"] == ag]
                     t_ag = len(df_ag)
                     d_ag = len(df_ag[df_ag["Statut_Delai"] == "Transmis dans le délai"])

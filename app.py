ae39c62e5a666c302171d7a5e55c94fc73210371
Clement Dalo <clementdalo56@gmail.com>
2026-09-06 09:32:14 +0000

    ltr

--- app.py
+++ app.py
@@ -334,8 +334,9 @@ else:
                         "Statut_Delai": "Transmis dans le délai", 
                         "Details": "Fichier Excel TCM déposé"
                     }
-                    pd.concat([df, pd.DataFrame([nouvelle_ligne])], ignore_index=True).to_csv(FICHIER_BDD, index=False)
-                    st.success("Excel archivé.")
+                            df_stats = pd.concat([df_stats, pd.DataFrame([nouvelle_ligne])], ignore_index=True)
+                             df_stats.to_csv(FICHIER_BDD, index=False)
+
 
     # --- SOUS-MENU 7 : CAHIER D'OBSERVATIONS ---
     elif choix_menu == "📝 Qualité & Justifications Hors Délai":
